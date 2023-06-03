1dafb157de9d4e0072c3d39b34a729cad203f9cc
Dmitry Zakharov <zakharov@ie.ibm.com>
2023-06-03 01:05:40 +0100

    test class

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 chmod 777 $0
 
-java -jar target/mesh-all.jar 
+java=$(which java11)
+
+GDK_SCALE=2 $java -jar target/mesh-all.jar 
---
+++ src/test/java/test/TestTreeset.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class TestTreeset {
+
+	@Test
+	public void test() {
+		int k = 3;
+		int[] arr = new int[] { 4, 6, 1, 8, 0, 2 };
+		int result = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			result = arr[i];
+			int cnt = 0;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (result > arr[j]) {
+					cnt++;
+					if (cnt > k) {
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(result);
+	}
+
+}
